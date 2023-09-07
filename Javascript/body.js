5cda31c0b4899568eea675c199121e70c8c6bc1c
Le Quoc Khanh <lequockhanh2505@gmail.com>
2023-09-07 16:31:18 +0700

    test5

---
+++ Javascript/body.js
@@ -0,0 +1,30 @@
+import products from "./data.js";
+
+console.log(products);
+
+const container = document.querySelector(".container");
+
+products.forEach((product) => {
+  const item = document.createElement("div");
+  item.classList.add("item");
+
+  const img = document.createElement("img");
+  img.src = product.imageSrc;
+  img.alt = "";
+
+  const heartIcon = document.createElement("span");
+  heartIcon.innerHTML = '<i class="fa-regular fa-heart"></i>';
+
+  const nameParagraph = document.createElement("p");
+  nameParagraph.textContent = product.name;
+
+  const priceParagraph = document.createElement("p");
+  priceParagraph.textContent = product.price;
+
+  item.appendChild(img);
+  item.appendChild(heartIcon);
+  item.appendChild(nameParagraph);
+  item.appendChild(priceParagraph);
+
+  container.appendChild(item);
+});
---
+++ Javascript/data.js
@@ -0,0 +1,54 @@
+const products = [
+  {
+    name: "14ProMax",
+    price: "20.000.000d",
+    imageSrc: "image/product/IP 14/ip 14 Promax.png",
+  },
+  {
+    name: "14ProMax",
+    price: "20.000.000d",
+    imageSrc: "image/product/IP 14/ip 14 Promax.png",
+  },
+  {
+    name: "14ProMax",
+    price: "20.000.000d",
+    imageSrc: "image/product/IP 14/ip 14 Promax.png",
+  },
+  {
+    name: "14ProMax",
+    price: "20.000.000d",
+    imageSrc: "image/product/IP 14/ip 14 Promax.png",
+  },
+  {
+    name: "14ProMax",
+    price: "20.000.000d",
+    imageSrc: "image/product/IP 14/ip 14 Promax.png",
+  },
+  {
+    name: "14ProMax",
+    price: "20.000.000d",
+    imageSrc: "image/product/IP 14/ip 14 Promax.png",
+  },
+  {
+    name: "14ProMax",
+    price: "20.000.000d",
+    imageSrc: "image/product/IP 14/ip 14 Promax.png",
+  },
+  {
+    name: "14ProMax",
+    price: "20.000.000d",
+    imageSrc: "image/product/IP 14/ip 14 Promax.png",
+  },
+  {
+    name: "14ProMax",
+    price: "20.000.000d",
+    imageSrc: "image/product/IP 14/ip 14 Promax.png",
+  },
+  {
+    name: "14ProMax",
+    price: "20.000.000d",
+    imageSrc: "image/product/IP 14/ip 14 Promax.png",
+  },
+];
+
+export default products;
