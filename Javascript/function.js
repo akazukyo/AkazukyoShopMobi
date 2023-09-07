7bbe67a332444c90fa5bf7740bdd0fe5c56fef74
Le Quoc Khanh <lequockhanh2505@gmail.com>
2023-09-07 12:25:17 +0700

    test4

--- Javascript/function.js
+++ Javascript/function.js
@@ -54,7 +54,7 @@ function find() {
     if (nav_logo_find.contains(event.target) && !bolshowFind) {
       nav_logo_find_form.style.display = "flex";
       setTimeout(function () {
-        inputElement.style.width = "150px";
+        inputElement.style.width = "120px";
         bolshowFind = true;
       }, 300);
       inputElement.style.animation = "ani_showFind";
