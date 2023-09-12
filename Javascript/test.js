3c611c0a567cd15a2589a191bc1d4764695c70ac
Le Quoc Khanh <lequockhanh2505@gmail.com>
2023-09-13 02:54:42 +0700

    test 5

--- Javascript/test.js
+++ Javascript/test.js
@@ -2,17 +2,21 @@ var imgClass = document.querySelector("div .image-class");
 var listProduct = document.getElementById("listProduct");
 var showImg = document.querySelector(".showIMG");
 var showAniImg = document.querySelector(".showIMGAnimation");
+var animationRun = false;
 
 listProduct.addEventListener("click", function (e) {
   if (
     e.target.id != "listProduct" &&
-    showImg.src != e.target.getAttribute("src")
+    showImg.src != e.target.getAttribute("src") &&
+    !animationRun
   ) {
     setTimeout(function () {
       showImg.src = e.target.getAttribute("src");
       showAniImg.classList.remove("active");
+      animationRun = false;
     }, 1500);
     showAniImg.src = e.target.getAttribute("src");
     showAniImg.classList.add("active");
+    animationRun = true;
   }
 });
