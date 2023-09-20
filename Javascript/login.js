e04c7fb39f542e198182a6cc9bebe7a28560a071
Le Quoc Khanh <lequockhanh2505@gmail.com>
2023-09-20 17:28:13 +0700

    test5

--- Javascript/login.js
+++ Javascript/login.js
@@ -5,7 +5,11 @@ test2 = document.querySelector(".field-pass:first-child");
 showPass.addEventListener("click", function (e) {
   if (inputPassword.type == "password") {
     inputPassword.type = "text";
+    showPass.children.item(0).classList.add("hide");
+    showPass.children.item(1).classList.remove("hide");
   } else {
     inputPassword.type = "password";
+    showPass.children.item(0).classList.remove("hide");
+    showPass.children.item(1).classList.add("hide");
   }
 });
